7cfb9b0538905581f3cc1936714bc33ac5967bab
Kacper <kacper@slezak>
2023-12-02 21:46:42 +0100

    python course

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print('Starting python course')
+line01 = "****************************"
+line02 = "*         HELLO!!!         *"
+line03 = "*      WELCOME HERE!!      *"
+line04 = "*    What is your name?    *"
+name = ""
+print(line01)
+print(line02)
+print(line03)
+print(line04)
+print("*          " + input(name) + "            *")
